52b8c630c95d5b646cbb88da21c7e58b58394426
Kenzie Togami <ket1999@gmail.com>
2019-10-05 22:32:17 -0700

    Make project compile, cleanup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.techshroom.inciseblue.commonLib
-
 plugins {
     id("org.rivierarobotics.gradlerioredux") version "0.6.0"
 }
@@ -7,16 +5,4 @@ plugins {
 gradleRioRedux {
     robotClass = "org.rivierarobotics.robot.Robot"
     teamNumber = 5818
-}
-
-dependencies {
-	implementation("org.slf4j:slf4j-api:1.7.25")
-	commonLib("ch.qos.logback", "logback", "1.2.3") {
-		implementation(lib("classic"))
-		implementation(lib("core"))
-	}
-}
-
-repositories {
-	jcenter()
 }
