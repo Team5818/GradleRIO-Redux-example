ee0c54bd8d9ab89734ca357faad29d64b3e27bac
Boomaa23 <cmo93003@yahoo.com>
2020-08-10 20:01:01 -0700

    Update dependencies and Gradle for 2020

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
-    id("org.rivierarobotics.gradlerioredux") version "0.7.0"
+    id("org.rivierarobotics.gradlerioredux") version "0.7.6"
 }
 
 gradleRioRedux {
     robotClass = "org.rivierarobotics.robot.Robot"
     teamNumber = 5818
+}
+
+repositories {
+    jcenter()
 }
