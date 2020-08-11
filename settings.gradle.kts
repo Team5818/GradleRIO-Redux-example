64219979077ae3153bd97e07f0b113b90fd49cc2
Octavia Togami <octavia.togami@gmail.com>
2020-08-10 20:19:39 -0700

    Don't be dumb and include GRR

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,1 @@
 rootProject.name = "GradleRIO-Redux-example"
-
-includeBuild("../GradleRIO-Redux") {
-    dependencySubstitution {
-        substitute(module("gradle.plugin.org.rivierarobotics:GradleRIO-Redux")).with(project(":"))
-    }
-}
